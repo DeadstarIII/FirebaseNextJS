6d2d0d33d20349ef044abbb11998f391d8fb9a60
deadstar <74278992+DeadstarIII@users.noreply.github.com>
2022-09-14 23:10:16 +0530

    Initial commit

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
 import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
 
-const clientCredintials = {
+const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -11,7 +9,9 @@ const clientCredintials = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({});
-}
-export default firebase;
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
