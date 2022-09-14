9658388440e14592ff54bd7945bde60e7fbe4c46
deadstar <74278992+DeadstarIII@users.noreply.github.com>
2022-09-14 22:56:51 +0530

    Initial commit

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,27 +1,5 @@
-import React from "react";
-import { StyledFirebaseAuth } from "react-firebaseui/StyledFirebaseAuth";
-import firebase from "../firebase/clientApp.js";
-
-const uiConfig = {
-  signInFlow: 'popup',
-  signInSuccessUrl: "/",
-  signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID],
-};
-
-function Auth() {
-  return (
-    <div
-      style={{
-        maWidth: "320px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <h1>Please Login</h1>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-    </div>
-  );
+function auth() {
+  return <div>auth</div>;
 }
-export default Auth;
+
+export default auth;
